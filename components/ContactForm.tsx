bbb8a696e2138834b04e066321b8a93f80bf0d5a
mmdz21 <mmohamadzz294@gmail.com>
2025-08-28 01:07:07 -0700

    flexible

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,7 +2,7 @@ import { sendMessage } from "@/app/actions/sendMessage";
 
 export function ContactForm() {
   return (
-    <section className="section flex flex-col items-center relative bg-[url('/images/bg-form.svg')] bg-cover bg-no-repeat bg-center">
+    <section className="section container flex flex-col items-center relative bg-[url('/images/bg-form.svg')] bg-cover bg-no-repeat bg-center">
       <div className="max-w-2xl space-y-8">
         <div className="relative text-center px-8">
           <h2 className="section-title text-white">
